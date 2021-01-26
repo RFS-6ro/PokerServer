07a120c17e0ff583ecba235165e3df832d50e6ba
RFS-6ro <56915388+RFS-6ro@users.noreply.github.com>
2021-01-26 12:40:19 +0200

    Reconnecting to other server works well

--- PokerServer/GameServer/ServerHandle.cs
+++ PokerServer/GameServer/ServerHandle.cs
@@ -10,12 +10,11 @@ namespace GameServer
 			int clientIdCheck = packet.ReadInt();
 			string username = packet.ReadString();
 
-			Console.WriteLine($"{IServer.Clients[fromClient].Tcp.Socket.Client.RemoteEndPoint} connected successfully and is now player {fromClient}.");
+			Console.WriteLine($"{IServer.Clients[fromClient].Tcp.Socket.Client.RemoteEndPoint} connected successfully and is now player {fromClient} with name {username}.");
 			if (fromClient != clientIdCheck)
 			{
 				Console.WriteLine($"Player \"{username}\" (ID: {fromClient}) has assumed the wrong client ID ({clientIdCheck})!");
 			}
-			//PokerSynchronisation.ServerPacketsSend.ConnectToOtherServer(fromClient, new ServerIdentifierData(NetworkSettings.LOCAL_HOST_IP, NetworkSettings.PORT), ServerSendHandlers.SendTCPData);
 		}
 	}
 }
--- PokerServer/PokerServer/ServerHandle.cs
+++ PokerServer/PokerServer/ServerHandle.cs
@@ -5,19 +5,21 @@ namespace FrontendServer
 {
 	public static class ServerHandle
 	{
-		internal static void WelcomeReceived(int fromClient, Packet packet)
+		public static void WelcomeReceived(int fromClient, Packet packet)
 		{
 			int clientIdCheck = packet.ReadInt();
 			string username = packet.ReadString();
 
-			Console.WriteLine($"{IServer.Clients[fromClient].Tcp.Socket.Client.RemoteEndPoint} connected successfully and is now player {fromClient}.");
+			Console.WriteLine($"{IServer.Clients[fromClient].Tcp.Socket.Client.RemoteEndPoint} connected successfully and is now player {fromClient} with name {username}.");
 			if (fromClient != clientIdCheck)
 			{
 				Console.WriteLine($"Player \"{username}\" (ID: {fromClient}) has assumed the wrong client ID ({clientIdCheck})!");
 			}
 
 			PokerSynchronisation.ServerPacketsSend.ConnectToOtherServer(fromClient, ServerPoolHandler.GetClosestServer(fromClient), ServerSendHandlers.SendTCPData);
-			//IServer.Clients[fromClient].Disconnect();
+			Console.WriteLine($"Player \"{username}\" (ID: {fromClient}) is disconnecting from Frontend server");
+			GC.SuppressFinalize(IServer.Clients[fromClient]);
+			IServer.Clients[fromClient] = new FrontendClient(fromClient);
 		}
 	}
 }
